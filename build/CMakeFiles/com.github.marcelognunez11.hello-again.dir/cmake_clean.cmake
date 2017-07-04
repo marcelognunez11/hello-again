788fe01ac4842c48e0629c3f717b3adc8d91478f
marcelognunez <marcelognunez@gmail.com>
2017-07-04 15:08:09 -0300

    build

---
+++ build/CMakeFiles/com.github.marcelognunez11.hello-again.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/hello-again.c"
+  "com.github.marcelognunez11.hello-again_valac.stamp"
+  "CMakeFiles/com.github.marcelognunez11.hello-again.dir/src/hello-again.c.o"
+  "com.github.marcelognunez11.hello-again.pdb"
+  "com.github.marcelognunez11.hello-again"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.marcelognunez11.hello-again.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
